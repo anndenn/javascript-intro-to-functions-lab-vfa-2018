ef3d39cab6045f1438b1d520753f7683ff1f7f1c
Anna Dennis <anndenn17@gmail.com>
2018-06-18 16:39:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function whisper(string) {
   return string.toLowerCase ()
 }
 function logShout(string) {
-  console.log (string.toUpperCase)
+  console.log (UpperCase)
 }
 function logShout(string) {
-  console.log (string.toLowerCase)
+  console.log (LowerCase)
 }
