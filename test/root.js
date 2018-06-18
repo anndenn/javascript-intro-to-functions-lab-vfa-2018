875a44b6372f6ae271a0944391de40df293d0870
Anna Dennis <anndenn17@gmail.com>
2018-06-18 17:48:07 +0000

    automatically backed up by learn

--- test/root.js
+++ test/root.js
@@ -13,7 +13,10 @@ before(function(done) {
 
   const html = path.resolve(__dirname, '..', 'index.html')
 
-  
+  jsdom.env('<div></div>', [], {
++    src: babelResult.code,
++    virtualConsole: jsdom.createVirtualConsole().sendTo(console)
++  }, (err, window) => {
     if (err) {
       return done(err);
     }
