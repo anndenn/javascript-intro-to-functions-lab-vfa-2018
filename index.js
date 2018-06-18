503fdeb3420072ff703d326529c3149507379938
Anna Dennis <anndenn17@gmail.com>
2018-06-18 17:36:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }else if (string === string.toUpperCase()) {
     return "YES INDEED!"
-  }else if (string === "I Love You, Grandma.)
+  }else if (string === "I Love You, Grandma.) {
+    
+  }
   }
     
   }
